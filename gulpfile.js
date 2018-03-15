f18709f3d77af5c88eb85053178a54c8b9498eb3
ninjaPixel <matthew@ninjaPixel.eu>
2018-03-15 14:37:49 +0000

    Clean packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,23 @@
-// var gulp = require("gulp");
-// var ts = require("gulp-typescript");
-// var tsProject = ts.createProject("tsconfig.json");
-//
-// gulp.task("default", function () {
-//     return tsProject.src()
-//         .pipe(tsProject())
-//         .js.pipe(gulp.dest("dist"));
-// });
-
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var tsify = require('tsify');
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
 var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
-var paths = {
-    ts: ['src/*.ts']
-};
+var babel = require('gulp-babel');
 
-// gulp.task('copyHtml', function () {
-//     return gulp.src(paths.pages)
-//         .pipe(gulp.dest('dist'));
-// });
 
-gulp.task('default', function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/index.ts'],
-        cache: {},
-        packageCache: {}
-    })
-    .plugin(tsify)
-    .transform('babelify', {
-        presets: ['es2015'],
-        extensions: ['.ts']
-    })
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("build", function () {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest("build"));
+});
+
+gulp.task("default", ["build"], function () {
+  return gulp.src('build/**/*')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+            presets: ['@babel/env']
+        }))
+    .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('dist'));
 });
