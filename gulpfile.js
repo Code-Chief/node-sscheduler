309aeaef9f90e019a6d511b682feb37a0d3b3269
ninjaPixel <matthew@ninjaPixel.eu>
2018-03-15 15:10:35 +0000

    Uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
-
-
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
 var tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("build", function () {
@@ -12,12 +12,23 @@ gulp.task("build", function () {
     .js.pipe(gulp.dest("build"));
 });
 
-gulp.task("default", ["build"], function () {
+gulp.task("es5", ["build"], function () {
   return gulp.src('build/**/*')
     .pipe(sourcemaps.init())
     .pipe(babel({
-            presets: ['@babel/env']
-        }))
+      presets: ['@babel/env']
+    }))
     .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('es5'));
+});
+
+gulp.task("default", ["es5"],function () {
+  return gulp.src('es5/**/*.js')
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify({mangle: true}))
+    .pipe(sourcemaps.write('.'))
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    })
+    .pipe(gulp.dest("dist"));
 });
